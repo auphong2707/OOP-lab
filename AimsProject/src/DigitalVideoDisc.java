383beb5dcc1ed2273312b5db63cd47f2563b8c29
Veil <65228307+auphong2707@users.noreply.github.com>
2024-04-27 09:48:56 +0700

    Update DigitalVideoDisc.java
    - Fix constructors order
    - Update ID per created object

--- AimsProject/src/DigitalVideoDisc.java
+++ AimsProject/src/DigitalVideoDisc.java
@@ -11,18 +11,6 @@ public class DigitalVideoDisc {
 	
 	private int id;
 	
-	public DigitalVideoDisc(String title) {
-		super();
-		this.title = title;
-	}
-	
-	public DigitalVideoDisc(String title, String category, float cost) {
-		super();
-		this.title = title;
-		this.category = category;
-		this.cost = cost;
-	}
-	
 	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
 		super();
 		this.title = title;
@@ -30,16 +18,23 @@ public class DigitalVideoDisc {
 		this.director = director;
 		this.length = length;
 		this.cost = cost;
+		
+		this.id = nbDigitalVideoDiscs++;
 	}
-
+	
 	public DigitalVideoDisc(String title, String category, String director, float cost) {
-		super();
-		this.title = title;
-		this.category = category;
-		this.director = director;
-		this.cost = cost;
+		this(title, category, director, 0, cost);
 	}
-
+	
+	public DigitalVideoDisc(String title, String category, float cost) {
+		this(title, category, null, 0, cost);
+	}
+	
+	public DigitalVideoDisc(String title) {
+		this(title, null, null, 0, 0.0f);
+	}
+	
+	
 	public String getTitle() {
 		return title;
 	}
