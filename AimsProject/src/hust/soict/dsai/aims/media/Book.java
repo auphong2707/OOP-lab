5a73a6e3f1b19a6cb13d1059664483e7f2341315
Veil <65228307+auphong2707@users.noreply.github.com>
2024-05-15 07:50:55 +0700

    Create Book.java

---
+++ AimsProject/src/hust/soict/dsai/aims/media/Book.java
@@ -0,0 +1,33 @@
+package hust.soict.dsai.aims.media;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Book extends Media{
+	private List<String> authors = new ArrayList<String>();
+	
+	public Book(int id, String title, String category, float cost) {
+		super(id, title, category, cost);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void addAuthor(String authorName) {
+		if(authors.contains(authorName)) {
+			System.out.println("Already contained");
+		}
+		else {
+			authors.add(authorName);
+			System.out.println("Added successfully!");
+		}
+	}
+	
+	public void removeAuthor(String anotherName) {
+		if(authors.contains(anotherName)) {
+			System.out.println("There is no author " + anotherName + " in the list!");
+		}
+		else {
+			authors.remove(anotherName);
+			System.out.println("Removed successfully!");
+		}
+	}
+}
