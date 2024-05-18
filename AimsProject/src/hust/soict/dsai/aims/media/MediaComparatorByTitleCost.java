54fe45c84db64482ec9d4caacfd4a0885a4a4162
Veil <65228307+auphong2707@users.noreply.github.com>
2024-05-18 17:06:59 +0700

    Add comparator

---
+++ AimsProject/src/hust/soict/dsai/aims/media/MediaComparatorByTitleCost.java
@@ -0,0 +1,14 @@
+package hust.soict.dsai.aims.media;
+
+import java.util.Comparator;
+
+public class MediaComparatorByTitleCost implements Comparator<Media> {
+
+	@Override
+	public int compare(Media o1, Media o2) {
+		return Comparator.comparing(Media::getTitle)
+				 		 .thenComparing(Comparator.comparing(Media::getCost).reversed())
+				 		 .compare(o1, o2);
+	}
+
+}
