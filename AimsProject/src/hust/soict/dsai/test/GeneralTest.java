3095f80f6a63f5a9c2942247b52e1c9a2b782d41
Veil <65228307+auphong2707@users.noreply.github.com>
2024-05-18 17:07:04 +0700

    Test

--- AimsProject/src/hust/soict/dsai/test/GeneralTest.java
+++ AimsProject/src/hust/soict/dsai/test/GeneralTest.java
@@ -20,14 +20,20 @@ public class GeneralTest {
 		testTracks.add(track3);
 		
 		CompactDisc cd = new CompactDisc(0, "Test CD", "Pop", "Yoasobi", 3, 5, "Yoasobi", testTracks);
+		CompactDisc cd2 = new CompactDisc(0, "Test CD", "Pop", "Yoasobi", 3, 6, "Yoasobi", testTracks);
+		CompactDisc cd3 = new CompactDisc(0, "Est CD", "Pop", "Yoasobi", 3, 5, "Yoasobi", testTracks);
 		DigitalVideoDisc dvd = new DigitalVideoDisc("The lion king", "Animation", "Roger Allers", 87, 19.95f);
 		Book book = new Book(0, "Book 1", "Pop", 15);
 		
 		List<Media> mediae = new ArrayList<Media>();
 		
 		mediae.add(cd);
+		mediae.add(cd2);
+		mediae.add(cd3);
 		mediae.add(dvd);
 		mediae.add(book);
+		
+		mediae.sort(Media.COMPARE_BY_COST_TITLE);
 		
 		for(Media m : mediae) {
 			System.out.println(m.toString());
