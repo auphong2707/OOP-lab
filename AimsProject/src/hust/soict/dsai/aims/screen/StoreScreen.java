8824843e290dad2946c2be1982d8e0b482e37c45
Veil <65228307+auphong2707@users.noreply.github.com>
2024-06-01 16:57:29 +0700

    Update Main

--- AimsProject/src/hust/soict/dsai/aims/screen/StoreScreen.java
+++ AimsProject/src/hust/soict/dsai/aims/screen/StoreScreen.java
@@ -244,22 +244,4 @@ public class StoreScreen extends JFrame{
 			addToCartDialog.setVisible(true);
 		}
 	}
-	
-	public static void main(String[] args) {
-		Store store = new Store();
-		Cart cart = new Cart();
-		
-		store.addMedia(new DigitalVideoDisc("Mashle"));
-		store.addMedia(new DigitalVideoDisc("Re:zero"));
-		store.addMedia(new DigitalVideoDisc("AoT"));
-		store.addMedia(new DigitalVideoDisc("Naruto"));
-		store.addMedia(new DigitalVideoDisc("Boruto"));
-		store.addMedia(new DigitalVideoDisc("Jujutsu Kaisen"));
-		store.addMedia(new DigitalVideoDisc("Dungeon Meshi"));
-		store.addMedia(new DigitalVideoDisc("Honkai"));
-		store.addMedia(new Book("Genshin", "Action", 12));
-		store.addMedia(new DigitalVideoDisc("Mashle 2"));
-		
-		new StoreScreen(store, cart);
-	}
 }
