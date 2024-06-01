316f76b9285512ca276b927e52f276198ee8cac5
Veil <65228307+auphong2707@users.noreply.github.com>
2024-06-01 14:27:01 +0700

    Remove Button

--- AimsProject/src/hust/soict/dsai/aims/screen/CartScreenController.java
+++ AimsProject/src/hust/soict/dsai/aims/screen/CartScreenController.java
@@ -5,6 +5,7 @@ import hust.soict.dsai.aims.media.Media;
 import hust.soict.dsai.aims.media.Playable;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -73,4 +74,11 @@ public class CartScreenController {
 			btnPlay.setVisible(false);
 		}
 	}
+	
+	@FXML
+	void btnRemovePressed(ActionEvent event) {
+		Media media = tbMedia.getSelectionModel().getSelectedItem();
+		cart.removeMedia(media);
+	}
+	
 }
