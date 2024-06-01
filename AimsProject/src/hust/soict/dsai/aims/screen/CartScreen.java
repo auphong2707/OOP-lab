6a0085fcb58993d90697c04a269b3d41aeb0f81f
Veil <65228307+auphong2707@users.noreply.github.com>
2024-06-01 13:40:56 +0700

    Update CartScreen.java

--- AimsProject/src/hust/soict/dsai/aims/screen/CartScreen.java
+++ AimsProject/src/hust/soict/dsai/aims/screen/CartScreen.java
@@ -1,10 +1,16 @@
 package hust.soict.dsai.aims.screen;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JFrame;
 
 import hust.soict.dsai.aims.cart.Cart;
+import hust.soict.dsai.aims.media.Book;
+import hust.soict.dsai.aims.media.CompactDisc;
+import hust.soict.dsai.aims.media.DigitalVideoDisc;
+import hust.soict.dsai.aims.media.Track;
 import javafx.application.Platform;
 import javafx.embed.swing.JFXPanel;
 import javafx.fxml.FXMLLoader;
@@ -23,6 +29,8 @@ public class CartScreen extends JFrame{
 		
 		this.setTitle("Cart");
 		this.setVisible(true);
+		this.setSize(1024, 768);
+		this.setLocationRelativeTo(null);
 		
 		Platform.runLater(new Runnable() {
 
@@ -30,16 +38,40 @@ public class CartScreen extends JFrame{
 			public void run() {
 				try {
 					FXMLLoader loader = new FXMLLoader(getClass()
-							.getResource("/screen/fxml/cart.fxml"));
+							.getResource("cart.fxml"));
 					CartScreenController controller = new CartScreenController(cart);
 					loader.setController(controller);
 					Parent root = loader.load();
-					fxPanel.setScene(new Scene(root));
+					Scene rootScene = new Scene(root);
+					
+					fxPanel.setScene(rootScene);
 				} catch(IOException e) {
 					e.printStackTrace();
 				}
 			}
 			
 		});
+	}
+	
+	public static void main(String[] args) {
+		DigitalVideoDisc dvd = new DigitalVideoDisc("Jujutsu Kaisen");
+		Book book = new Book("Gun and Chocolate", "Slice of Life", 12);
+		
+		List<Track> testTracks = new ArrayList<Track>();
+		Track track1 = new Track("Monster", 3);
+		Track track2 = new Track("Idol", 4);
+		Track track3 = new Track("Heart Beat", 6);
+		testTracks.add(track1);
+		testTracks.add(track2);
+		testTracks.add(track3);
+		CompactDisc cd = new CompactDisc("Test CD", "Pop", "Yoasobi", 3, 5, "Yoasobi", testTracks);
+		
+		
+		Cart cart = new Cart();
+		cart.addMedia(dvd);
+		cart.addMedia(book);
+		cart.addMedia(cd);
+		
+		new CartScreen(cart);
 	}
 }
